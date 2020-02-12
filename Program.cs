e696af645fb3c287a8316830c7848dff60f09038
Ty-Meeks <tyrmeeks@gmail.com>
2020-02-12 11:30:09 -0500

    Updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace GroupProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var planet = new Planet();
+            Console.WriteLine(Planet.Name);
+
+            
         }
     }
+
+    class Planet
+    {
+        public string Name;
+        public int Size;
+        public int DistancefromStar;
+        public int Population;
+        public string Climate;
+        public int Atmoshphere;
+        public string TradeSupply;
+        public string TradeDemand;
+    }
+    class SpaceStation
+    {
+        public string Name;
+        public int Size;
+        public int DistancefromStar;
+        public int Population;
+        public string TradeSupply;
+        public string TradeDemand;
+    }
 }
+    
