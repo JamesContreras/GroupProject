d62dfabbe3d89d78520b29629392b81c59470022
James Contreras <james.a.contreras@outlook.com>
2020-02-18 08:38:10 -0500

    added stuff

--- Program.cs
+++ Program.cs
@@ -2,41 +2,105 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Linq;
+using System.Numerics;
 
 namespace GroupProject
 {
     class Program
     {
-        private static string[] Map;
+        public static List<string> PlanetList = new List<string> { };
+        public static int PlanetOrder = 0;
+        
 
         public static void Main(string[] args)
         {
 
-
-            Display();
-            Planets();
-            SpaceStation();   
-
-            GameTitle();
-            StartGame();
-            EndGame();
-            GameOver();
-            YouWin(); 
-            Credits();
             
+            GameTitle();
+            
+            
+
+        }
+
+        public static void MainMenuShow(string input)
+        {
+            switch (input)
+            {
+                case "Enter":
+
+
+                    break;
+
+                case "L":
+
+                    ImportPlanetList();
+
+                    break;
+
+                case "I":
+
+                    Credits();
+
+                    break;
+
+                default:
+                    break;
+            }
+
+               
+        }
+
+        public static void PortMenu(string input)
+        {
+            switch(input)
+            {
+                case "B":
+                    Console.WriteLine("What are you buying?");
+                    
+                    break;
+
+            }
+        }
+
+        public static void PlanetInteract(string input)
+        {
+            switch(input)
+            {
+                
+            }
         }
         
+
+        private static void ImportPlanetList()
+        {
+            PlanetList = System.IO.File.ReadAllLines(@"C:\Users\Texas\source\repos\GroupProject\PlanetList.txt").ToList();
+        }
+
+        public static void SaveGame()
+        {
+           System.IO.File.WriteAllLines(@"C:\Users\Texas\source\repos\GroupProject\PlanetList.txt", PlanetList);
+        }
+
+        public static void Display()
+        {
+            int showplanet = 5;
+            for (int i = 0; i < showplanet; i++)
+            {
+
+            }
+        }
+
+       
+
         public static void GameTitle()
         {
-            Console.WriteLine("Welcome to SpaceX World");
-            Console.WriteLine("Press 'Enter' to Begin");
-            Console.ReadLine();
+            Console.WriteLine("WELCOME TO SPACE X UNIVERSE");
+
+            MainMenu();
             Console.Clear();   
         }
-        public static void StartGame()
-        {
-            Console.WriteLine("Start the Game");
-        }
+       
         public static void EndGame()
         {
             Console.WriteLine("End of Game");
@@ -45,14 +109,7 @@ namespace GroupProject
             Console.ReadLine();
             Console.Clear();
         }
-        public static void GameOver()
-        {
-            Console.WriteLine("Game Over");
-            Console.WriteLine("Press 'X' to Exit");
-            Console.WriteLine("Press 'X' to Exit or 'Enter' to Begin Another Game");
-            Console.ReadLine();
-            Console.Clear();
-        }
+      
 
         public static void YouWin()
         {
@@ -67,6 +124,38 @@ namespace GroupProject
             Console.WriteLine("Ty");
             Console.ReadLine();
             Console.Clear();
+
+        }
+
+        public static void MainMenu()
+        {
+            Console.WriteLine("Press 'Enter' to begin");
+            Console.WriteLine("Type L : to Load last save");
+            Console.WriteLine("Type I : to see credits");
+        }
+
+        
+
+        public static void Menubuttons()
+        {
+           
+
+            Console.WriteLine("\nYou are resting easy at your current port. What would you like to do?");
+            Console.WriteLine("\nType B : Buy");
+            Console.WriteLine("\nType S : Sell");
+            Console.WriteLine("\nType I : Seek information");
+
+
+            
+        }
+
+        public static void PlanetChoice()
+        {
+            Console.WriteLine($"\nYou are currently at : ");
+            Console.WriteLine($"\n Type {2} : Moon");
+            Console.WriteLine($"\n Type {3} : Mars");
+            Console.WriteLine($"\n Type {4} : Hadley's Hope");
+            Console.WriteLine($"\n Type {5} : Ticonderoga");
 
         }
 
@@ -124,7 +213,7 @@ namespace GroupProject
         {
             var spaceship = new Ship();
             spaceship.ShipName = "Phoenix";
-            spaceship.CargoCap = 30;
+            spaceship.CargoCap = 1000;
             spaceship.ShipSpeed = 90;
         }
 
@@ -134,129 +223,10 @@ namespace GroupProject
             currency.SpaceXBucks = int.Parse(Console.ReadLine());
         }
 
-        
-
-
-        public static void Display(string input)
-        {
-            do
-            {
-
-                switch (input)
-                {
-                    case "1":
-
-                        Console.WriteLine(
-                    "|      Current Location: Earth    |" +
-                    "|                                 |" +
-                    "|   ( X )                         |" +
-                    "|                                 |" +
-                    "|                    (   )        |" +
-                    "|                                 |" +
-                    "|   ( )                           |" +
-                    "|                                 |" +
-                    "|                          (   )  |" +
-                    "|_________________________________|");
-
-
-                        break;
-                    case "2":
-                        Console.WriteLine(
-                    "|     Current Location: Moon      |" +
-                    "|     Star System: Sol            |" +
-                    "|   (   )                         |" +
-                    "|                                 |" +
-                    "|                    (   )        |" +
-                    "|                                 |" +
-                    "|   (X)                           |" +
-                    "|                                 |" +
-                    "|                          (   )  |" +
-                    "|_________________________________|");
-                        break;
-
-                    case "3":
-                        Console.WriteLine(
-                    "|     Current Location: Mars      |" +
-                    "|                                 |" +
-                    "|   (   )                         |" +
-                    "|                                 |" +
-                    "|                    ( X )        |" +
-                    "|                                 |" +
-                    "|   ( )                           |" +
-                    "|                                 |" +
-                    "|                          (   )  |" +
-                    "|_________________________________|");
-                        break;
-
-                    case "4":
-                        Console.WriteLine(
-                    "|Current Location : Hadley's Hope |" +
-                    "|                                 |" +
-                    "|   (   )                         |" +
-                    "|                                 |" +
-                    "|                    (   )        |" +
-                    "|                                 |" +
-                    "|   ( )                           |" +
-                    "|                                 |" +
-                    "|                          ( X )  |" +
-                    "|_________________________________|");
-                        break;
-
-                    case "5":
-                        Console.WriteLine(
-                    "|Current Location : Ticonderoga   |" +
-                    "|Star System: Alpha Centauri      |" +
-                    "|                                 |" +
-                    "|                                 |" +
-                    "|             ( X )               |" +
-                    "|                                 |" +
-                    "|                                 |" +
-                    "|                                 |" +
-                    "|                                 |" +
-                    "|_________________________________|");
-
-                     
-                        break;
-                }
-                Menubuttons();
-
-            } while (Age() <= 75);
-
-           
-        }
-
-        private static int Age()
-        {
-            if (Age = 75)
-            {
-                Console.ReadLine()
-            }
-          
-        }
-
-        public static void Menubuttons()
-        {
-            Console.WriteLine("\nYou are resting easy at your current port. What would you like to do?");
-            Console.WriteLine("\nType B : Buy");
-            Console.WriteLine("\nType S : Sell");
-            Console.WriteLine("\nType I : Seek information");
-
-            Display();
-        }
-
-        public static void PlanetChoice()
-        {
-            Console.WriteLine($"\nYou are currently at : " ;
-            Console.WriteLine($"\n Type {2} : Moon");
-            Console.WriteLine($"\n Type {3} : Mars");
-            Console.WriteLine($"\n Type {4} : Hadley's Hope");
-            Console.WriteLine($"\n Type {5} : Ticonderoga");
-
-            Display();
-        }
-
 
     }
+
+
 
 
 
